09ebd2c69d997a25d2570b100a39e2fbb14ceaac
Dipen Patel <patel.dip@husky.neu.edu>
2019-03-15 10:49:40 -0400

    Updated Lesson and Topic class

--- src/main/java/com/example/assignment6server/models/Lesson.java
+++ src/main/java/com/example/assignment6server/models/Lesson.java
@@ -1,11 +1,27 @@
 package com.example.assignment6server.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Lesson {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String title;
+  @ManyToOne(targetEntity = Module.class)
+  @JsonIgnore
+  private Module module;
+  @OneToMany(mappedBy = "lesson")
   private List<Topic> topics = new ArrayList<>();
 
   public Lesson() {
@@ -32,11 +48,24 @@ public class Lesson {
     this.title = title;
   }
 
+  public Module getModule() {
+    return module;
+  }
+
+  public void setModule(Module module) {
+    this.module = module;
+  }
+
   public List<Topic> getTopics() {
     return topics;
   }
 
   public void setTopics(List<Topic> topics) {
     this.topics = topics;
+  }
+
+  //our implementation which we use to update the Lesson
+  public void set(Lesson newLesson) {
+    this.title = newLesson.title;
   }
 }
